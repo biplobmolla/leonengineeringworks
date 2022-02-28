96b8854e79f732fa39fb9c9d0e9d5f687531a720
Biplob <ahmedtamim317@gmail.com>
2022-02-28 16:45:17 +0600

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 const menu = document.getElementById('hamburger');
 const close = document.getElementById('close');
 const nav = document.getElementById('nav');
+let link = document.querySelectorAll('nav a');
+
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', () => {
+        nav.style.right = "-120%";
+    });
+}
 
 menu.addEventListener('click', () => {
     nav.style.right = "0";
